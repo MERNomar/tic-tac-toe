ceaa9fcebc3bbf03f11bcf898dd6f12059fedb69
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-07-16 15:05:01 +0300

    adding data attributes
    adding data attributes

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let emptyCells = document.querySelectorAll(".emptyCell");
 let cellPlacehoder = true;
-
+const winCombos = [
+    [0,1,2],
+    [0,3,6],
+    [3,4,5],
+    [6,7,8],
+    [1,4,7],
+    [2,4,6],
+    [2,5,8],
+    [0,4,8]
+];
+let board = ["","","","","","","","",""]
 
 emptyCells.forEach(cell => cell.addEventListener('click' , e => {
-    if (cell.innerHTML === "X" || cell.innerHTML === "O" ) return
 
     if (cellPlacehoder === true){
         Xstate(cell)
@@ -14,18 +23,25 @@ emptyCells.forEach(cell => cell.addEventListener('click' , e => {
 
 
 function Xstate(cell){
+    if (cell.innerHTML === "X" || cell.innerHTML === "O" ) return
     cellPlacehoder = false;
     cell.innerHTML = "X";
+    console.log(cell)
 }
 
 function Ostate(cell){
+    if (cell.innerHTML === "X" || cell.innerHTML === "O" ) return
     cellPlacehoder = true;
     cell.innerHTML = "O";
     cell.style.color = "red"
+    console.log(cell)
+
 }
 
 
 emptyCells.forEach(cell => cell.addEventListener('click' , e => {
 }));
-console.log(emptyCells[0])
+
+
+
 
