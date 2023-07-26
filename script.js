7942982a68593077725886421a797b38e2a714f6
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-07-26 16:19:06 +0300

    adding board and win combos
    adding board and win combos and filling the board based on html data attribute

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 let emptyCells = document.querySelectorAll(".emptyCell");
 let cellPlacehoder = true;
-let board = ["","","","","","","","",""]
 let player = (marker , player) => {
     return {marker , player}
 }
@@ -17,17 +16,10 @@ emptyCells.forEach(cell => cell.addEventListener('click' , e => {
 }));    
 
 
+
+
+
 const winState = (() => {
-const winCombos = [
-    [0,1,2],
-    [0,3,6],
-    [3,4,5],
-    [6,7,8],
-    [1,4,7],
-    [2,4,6],
-    [2,5,8],
-    [0,4,8]
-];
 
 
 
@@ -39,10 +31,28 @@ const winCombos = [
 
 
 let markGrid = (() => {
+
+    const winCombos = [
+        [0,1,2],
+        [0,3,6],
+        [3,4,5],
+        [6,7,8],
+        [1,4,7],
+        [2,4,6],
+        [2,5,8],
+        [0,4,8]
+    ];
+    
+    let board = ["","","","","","","","",""]
+
 function Xstate(cell){
     if (cell.innerHTML === "X" || cell.innerHTML === "O" ) return
     cellPlacehoder = false;
     cell.innerHTML = "X";
+    console.log()
+    board[cell.dataset.key] = "X"
+    console.log(board)
+
 }
 
 function Ostate(cell){
@@ -50,7 +60,9 @@ function Ostate(cell){
     cellPlacehoder = true;
     cell.innerHTML = "O";
     cell.style.color = "red"
-
+    console.log(cell.dataset.key)
+    board[cell.dataset.key] = "O"
+    console.log(board)
 }
 
 function sayhi(){
