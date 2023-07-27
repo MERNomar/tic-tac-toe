3c4eaf91589d94a2ecf3936485a8fd8200ca3185
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-07-27 14:57:09 +0300

    fixing html data set and adding win state check

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+let playerTurn = document.querySelector(".player-turn");
 let emptyCells = document.querySelectorAll(".emptyCell");
 let cellPlacehoder = true;
 let player = (marker , player) => {
@@ -7,23 +7,14 @@ let player = (marker , player) => {
 
 // the event listener to manage the whole click thing
 emptyCells.forEach(cell => cell.addEventListener('click' , e => {
-
-    if (cellPlacehoder === true){
-        markGrid.Xstate(cell)
-    }else{
-        markGrid.Ostate(cell)
-    }
+    markGrid.markCell(cell)
 }));    
 
 
 
 
 
-const winState = (() => {
 
-
-
-})
 
 
 
@@ -31,7 +22,7 @@ const winState = (() => {
 
 
 let markGrid = (() => {
-
+    "use strict"
     const winCombos = [
         [0,1,2],
         [0,3,6],
@@ -45,14 +36,44 @@ let markGrid = (() => {
     
     let board = ["","","","","","","","",""]
 
+
+    function changeStyleX(){
+        playerTurn.style.color = "rgb(132, 132, 209)"
+        playerTurn.innerHTML = "Player X turn"
+    }
+
+    function changeStyleO(){
+        playerTurn.style.color = "brown"
+        playerTurn.innerHTML = "Player O turn"
+
+    }
+   
+    function checkWinState(){
+        for (let i = 0; i < winCombos.length; i++){
+            let companiton = winCombos[i]
+            let a = board[companiton[0]]
+            let b = board[companiton[1]]
+            let c = board[companiton[2]]
+            if (a == "" || b == "" || c == ""){
+                console.log("no matches")
+                continue
+                
+            }else if(a == b && b == c ){
+                console.log("winner")
+                if (a === "O") {console.log("player o wins")}
+                else {console.log("player x wins")}
+                break
+            }}}
+
+    
+
 function Xstate(cell){
     if (cell.innerHTML === "X" || cell.innerHTML === "O" ) return
     cellPlacehoder = false;
     cell.innerHTML = "X";
-    console.log()
     board[cell.dataset.key] = "X"
-    console.log(board)
-
+    checkWinState()
+    changeStyleO()
 }
 
 function Ostate(cell){
@@ -60,16 +81,34 @@ function Ostate(cell){
     cellPlacehoder = true;
     cell.innerHTML = "O";
     cell.style.color = "red"
-    console.log(cell.dataset.key)
     board[cell.dataset.key] = "O"
-    console.log(board)
-}
+    checkWinState()
+    changeStyleX()
+    }
 
-function sayhi(){
-console.log("hi")}
+
+    function markCell(cell){
+        if (cellPlacehoder === true){
+            Xstate(cell)
+        }else{
+            Ostate(cell)
+        }
+    }
+
 
 return {
-    Ostate , Xstate , sayhi
+    markCell
 }
 })();
+
+
+
+
+
+
+
+
+
+
+
 
